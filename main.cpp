dd83f7cee46f3353fb453e8e0ef0b560709c755f
Diogo Campos <diogotc2207@gmail.com>
2025-07-03 18:07:35 -0300

    v10

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
+/* TRABALHO PRATICO - BCC 221 - PROGRAMAÇÃO ORIENTADA À OBJETOS (POO)
+    SISTEMA DE CONTROLE DE VOO EM AEROPORTO
+    Diogo Tadeu Campos - 24.1.4003
+    Thiago Henrique de Oliveira Gonçalves - 24.1.4036
+    Wendel Cauã Silva de Oliveira - 24.1.4021
+*/
+
 #include <iostream>
 #include "include/sistema.h"
 #include "include/utilidades.h" 
